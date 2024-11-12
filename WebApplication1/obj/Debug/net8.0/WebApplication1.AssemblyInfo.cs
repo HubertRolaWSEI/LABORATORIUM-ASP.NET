37d7bb32d431c6e3b6c7c614b33113a0be4d5273
hubert.rola <hubert.rola1337@gmail.com>
2024-11-12 11:51:18 +0100

    commit

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0accb1ba4ced0b1c1798e7eeb1952291ecc4ed60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d96078936021fa68f211e93738f88ec3238ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
