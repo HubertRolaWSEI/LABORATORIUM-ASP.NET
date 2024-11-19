9830e9d1ac2da00f976fc3f43c2d248bdcd5a665
hubert.rola <hubert.rola1337@gmail.com>
2024-11-19 13:07:11 +0100

    commit

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,19 +1,37 @@
 using WebApplication1.Models;
 using WebApplication1.Models.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<AppDbContext>();
-builder.Services.AddTransient<iContactService, EFContactService>();
+builder.Services.AddDefaultIdentity<IdentityUser>(o =>
+    {
+        o.SignIn.RequireConfirmedAccount = true;
+        o.Password.RequiredLength = 5;
+        o.Password.RequireNonAlphanumeric = false;
+        o.Password.RequireUppercase = false;
+        o.Password.RequiredUniqueChars = 0;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
+
+builder.Services.AddTransient<iContactService, EFContactService>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,7 +40,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be1b1ba2b7a953a35601ca26f8f9373a99058e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a87b5544e00a61e10b7c41749f51cff26620a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
